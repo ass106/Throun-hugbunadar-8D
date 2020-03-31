b41a7c90244a6bb3d3ef41257c88039710a36a51
ass106 <43350367+ass106@users.noreply.github.com>
2020-03-31 01:14:13 +0000

    Removed TourDetails, decided on no split.
    UPDATED Tour.java as well, with getters!

--- Project/bookingModule/Tour.java
+++ Project/bookingModule/Tour.java
@@ -1,15 +1,143 @@
 package bookingModule;
 
+import java.util.ArrayList;
+
 public class Tour {
-    Operator operator;
-    String tourName;
-    String tourId;
-    Date date;
-    Time startTime;
-    int duration;
-    String region;
-    ArrayList<String> activities;
-    ArrayList<String> attractions;
-    String city;
-    int availability;
+    private Operator operator;
+    private String tourName;
+    private String tourId;
+    private Date date;
+    private Time startTime;
+    private int duration;
+    private String region;
+    private ArrayList<String> activities;
+    private ArrayList<String> attractions;
+    private String city;
+    private int availability;
+    private Boolean hotelPickup;
+    private ArrayList<String> itinerary;
+    private ArrayList<String> equipment;
+    private int difficulty;
+    private int minAge;
+    private ArrayList<String> languages;
+    private ArrayList<String> included;
+    private ArrayList<String> excluded;
+    private String otherInfo;
+    private Double price;
+
+    public Tour(Operator OPERATOR, String TOURNAME, String TOURID, Date DATE, Time STARTTIME, int DURATION,
+            String REGION, ArrayList<String> ACTIVITIES, ArrayList<String> ATTRACTIONS, String CITY, int AVAILABILITY,
+            Boolean HOTELPICKUP, ArrayList<String> ITINERARY, ArrayList<String> EQUIPMENT, int DIFFICULTY, int MINAGE,
+            ArrayList<String> LANGUAGES, ArrayList<String> INCLUDED, ArrayList<String> EXCLUDED, String OTHERINFO,
+            Double PRICE) {
+        this.operator = OPERATOR;
+        this.tourName = TOURNAME;
+        this.tourId = TOURID;
+        this.date = DATE;
+        this.startTime = STARTTIME;
+        this.duration = DURATION;
+        this.region = REGION;
+        this.activites = ACTIVITIES;
+        this.attractions = ATTRACTIONS;
+        this.city = CITY;
+        this.availability = AVAILABILITY;
+        this.hotelPickup = HOTELPICKUP;
+        this.itinerary = ITINERARY;
+        this.equipment = EQUIPMENT;
+        this.difficulty = DIFFICULTY;
+        this.minAge = MINAGE;
+        this.languages = LANGUAGES;
+        this.included = INCLUDED;
+        this.excluded = EXCLUDED;
+        this.otherInfo = OTHERINFO;
+        this.price = PRICE;
+    }
+
+    public String getOperator() {
+        return this.operator;
+    }
+
+    public String getTourName() {
+        return this.tourName;
+    }
+
+    public String getTourId() {
+        return this.tourId;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    public Time getStartTime() {
+        return this.startTime;
+    }
+
+    public int getDuration() {
+        return this.duration;
+    }
+
+    public String getRegion() {
+        return this.region;
+    }
+
+    public ArrayList<String> getActivities() {
+        return this.activities;
+    }
+
+    public String getAttractions() {
+        return this.attractions;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public String getAvailability() {
+        return this.availability;
+    }
+
+    public Boolean getHotelPickup() {
+        return this.hotelPickup;
+    }
+
+    public ArrayList<String> getItinerary() {
+        return this.itinerary;
+    }
+
+    public ArrayList<String> getEquipment() {
+        return this.equipment;
+    }
+
+    public int getDifficulty() {
+        return this.difficulty;
+    }
+
+    public int getMinAge() {
+        return this.minAge;
+    }
+
+    public ArrayList<String> getLanguages() {
+        return this.languages;
+    }
+
+    public ArrayList<String> getIncluded() {
+        return this.included;
+    }
+
+    public ArrayList<String> getExcluded() {
+        return this.excluded;
+    }
+
+    public String getOtherInfo() {
+        return this.otherInfo;
+    }
+
+    public Double getPrice() {
+        return this.price;
+    }
+
+    public void setAvailability(int pax) {
+        this.availability -= pax;
+    }
 }
