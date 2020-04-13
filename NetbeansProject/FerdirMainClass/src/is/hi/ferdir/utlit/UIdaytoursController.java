b54198a8d4bb562848f3004767d5485b176f16b0
teyter <teitur93@gmail.com>
2020-04-13 23:17:13 +0000

    FXML vidmit fra strada, asamt controller

---
+++ NetbeansProject/FerdirMainClass/src/is/hi/ferdir/utlit/UIdaytoursController.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package is.hi.ferdir.utlit;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+/**
+ * FXML Controller class
+ *
+ * @author Teitur <teg6@hi.is>
+ */
+public class UIdaytoursController implements Initializable {
+
+    @FXML
+    private Color x2;
+    @FXML
+    private Font x1;
+    @FXML
+    private Color x4;
+    @FXML
+    private Font x3;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
